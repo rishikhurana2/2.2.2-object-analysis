24ae9cf41c60968fda03fd8720b322b42195ba05
rishikhurana2 <rishikhurana2@gmail.com>
2018-10-20 00:20:43 -0700

    Khurana.Rishab

--- week3.py
+++ week3.py
@@ -10,40 +10,42 @@ class week3lab:
 	THRESHOLD_MIN = np.array([0,0,236], np.uint8)
 	THRESHOLD_MAX = np.array([255, 100, 255], np.uint8)
 	threshedframe = cv2.inRange(hsv, THRESHOLD_MIN, THRESHOLD_MAX)
-	count = -1;
+	count = -1
 	image, contours, hierarchy = cv2.findContours(threshedframe, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	cv2.drawContours(img, contours, -1, (255,0,0), 5)
-	cv2.imshow("Contours", img)
 	for cont in contours:
+		count = count + 1
 		approx = cv2.approxPolyDP(cont, 0.1*cv2.arcLength(cont, True), True)
-		for i in approx:
-			if i[0][0] > maxX:
-				maxX = i[0][0]
-			if i[0][0] < minX:
-				minX = i[0][0]
-			if i[0][1] > maxY:
-				maxY = i[0][1]
-			if i[0][1] < minY:
-				minY = i[0][1]
-		width = maxX-minX
-		height = maxY-minY
-		focalLength = 700
-		realWidth = 50
-		realHeight = 25
-		imgCenterX = (maxX + minX)/2
-		imgCenterY = (maxY + minY)/2
-		centerX = width/2
-		centerY = height/2
-		xOffset = imgCenterX - centerX
-		yOffset = imgCenterY - centerY
-	distance = (focalLength * realWidth)/width
-	azimuth = np.arctan(xOffset/focalLength)*180/np.pi
-	altitude = np.arctan(yOffset/focalLength)*180/np.pi
-	print(distance)
-	print(azimuth)
-	print(altitude)
-	print("The first one is distance")
-	print("The second one is azimuth")
-	print("The third one is altitude")
-	print("(For some reason I couldn't cacatinate a string with the distance, azimuth, and altitude)")
+		if (cv2.contourArea(approx) > 100):
+			cv2.drawContours(img, contours, count, (255,0,0), 5)
+			for i in approx:
+				if (i[0][0] > maxX):
+					maxX = i[0][0]
+				if (i[0][0] < minX):
+					minX = i[0][0]
+				if (i[0][1] > maxY):
+					maxY = i[0][1]
+				if (i[0][1] < minY):
+					minY = i[0][1]
+			width = maxX-minX
+			height = maxY-minY
+			focalLength = 700
+			realWidth = 50
+			realHeight = 25
+			imgCenterX = (maxX + minX)/2
+			imgCenterY = (maxY + minY)/2
+			centerX = width/2
+			centerY = height/2
+			xOffset = hsv.shape[0]-imgCenterX
+			yOffset = hsv.shape[1]-imgCenterY
+			distance = (focalLength * realWidth)/width
+			azimuth = np.arctan(xOffset/focalLength)*180/np.pi
+			altitude = np.arctan(yOffset/focalLength)*180/np.pi
+			print(distance)
+			print(azimuth)
+			print(altitude)
+			print("The first one is distance")
+			print("The second one is azimuth")
+			print("The third one is altitude")
+			print("(For some reason I couldn't cacatinate a string with the distance, azimuth, and altitude)")
+	cv2.imshow("Contour Image", img)
 	cv2.waitKey(0)
